c66e01b7a100cbb6843678c5c2590382d3b65d6f
Chivu23 <chivu1312g@gmail.com>
2023-12-03 15:04:02 +0200

    ModuleNotFoundError: No module named 'flask_sqlalchemy'

---
+++ app.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False   # a warning msg doesn't appear when we run app
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'   # the location of database
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_kek=True)
+    name = db.Column(db.String(50))
+    location = db.Column(db.String(50))
+    date_created = db.Comn(db.DateTime, default=datetime.now)
+
